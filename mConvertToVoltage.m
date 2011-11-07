eb1bcde558e357c9718b401534125bc99fbe1aa7
Jared Kofron <jared.kofron@gmail.com>
2011-11-07 08:44:20 -0800

    added a function which converts events to a voltage representation instead of a digital representation

---
+++ mConvertToVoltage.m
@@ -0,0 +1,13 @@
+% mConvertToVoltage.m
+% written by jared kofron <jared.kofron@gmail.com>
+% takes a monarch event and transforms the raw digitizer
+% data into voltage values.
+function new_event = mConvertToVoltage(event)
+new_event = event;
+if isequal(event.data_type,'uint32')
+    bit_width = 2^8;
+    trans = @(x) -0.25 + (x/bit_width)*.5;
+    new_event.data = arrayfun(trans,new_event.data);
+end
+% otherwise do nothing
+end
