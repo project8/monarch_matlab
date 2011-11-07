68452835067b4572e6b49cabeb9cac09fd0ee313
Jared Kofron <jared.kofron@gmail.com>
2011-11-07 08:28:19 -0800

    switched to struct return value

--- mGetNextEvent.m
+++ mGetNextEvent.m
@@ -1,16 +1,17 @@
 % mGetNextEvent.m
 % Given an open egg, returns the next event in the file.
-function [id,ts,data] = mGetNextEvent(current_egg)
+function event = mGetNextEvent(current_egg)
+    event = struct();
 % read.
   [raw,nbytes] = fread(current_egg.handle,current_egg.record_size);
   if nbytes == 0
-      id = -1;
-      ts = -1;
-      data = [];
+      event.id = -1;
+      event.ts = -1;
+      event.data = [];
   else
       raw = transpose(raw);
-      id = typecast(raw(1:8),'uint32');
-      ts = typecast(raw(9:17),'uint32');
-      data = raw(18:end);
+      event.id = typecast(raw(1:8),'uint32');
+      event.ts = typecast(raw(9:17),'uint32');
+      event.data = raw(18:end);
   end
 end
